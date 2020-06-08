819bc821b2743fe9d44c0da4880e3d0c76c3cf1b
Priyanka Sachan <56231576+Priyanka-Sachan@users.noreply.github.com>
2020-06-08 14:25:48 +0530

    Add action and intent.

--- app/src/main/java/com/example/notify_1/MainActivity.java
+++ app/src/main/java/com/example/notify_1/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -13,6 +16,8 @@ import static com.example.notify_1.App.CHANNEL_1_ID;
 import static com.example.notify_1.App.CHANNEL_2_ID;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int NOTIFICATION_INTENT=1;
 
     //NotificationManagerCompat used here to allow backwards compatibility.
     NotificationManagerCompat notificationManagerCompat;
@@ -33,12 +38,26 @@ public class MainActivity extends AppCompatActivity {
         String Title=editTextTitle.getText().toString();
         String Message=editTextMessage.getText().toString();
 
+        //For opening activity
+        Intent activityIntent=new Intent(this,MainActivity.class);
+        PendingIntent contentIntent =PendingIntent.getActivity(this,NOTIFICATION_INTENT,activityIntent,0);
+
+        //For actions
+        Intent broadcastIntent=new Intent(this,NotificationReceiver.class);
+        broadcastIntent.putExtra("NOTIFICATION_MESSAGE",Message);
+        PendingIntent actionIntent=PendingIntent.getBroadcast(this,NOTIFICATION_INTENT,broadcastIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+
         //For api<26,notification attributes should also be defined here.
         Notification notification=new NotificationCompat.Builder(this,CHANNEL_1_ID)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setSmallIcon(R.drawable.ic_one)
                 .setContentTitle(Title)
                 .setContentText(Message)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setColor(Color.BLUE)
+                .setContentIntent(contentIntent)
+                .setAutoCancel(true)
+                .setOnlyAlertOnce(true)
+                .addAction(R.mipmap.ic_launcher_round,"TOAST",actionIntent)
                 .build();
         notificationManagerCompat.notify(1,notification);
     }
@@ -47,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         String Message=editTextMessage.getText().toString();
 
         Notification notification=new NotificationCompat.Builder(this,CHANNEL_2_ID)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setSmallIcon(R.drawable.ic_two)
                 .setContentTitle(Title)
                 .setContentText(Message)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
