f7e023da1bb16076234a9467a1222fd582927368
Priyanka Sachan <56231576+Priyanka-Sachan@users.noreply.github.com>
2020-06-08 15:22:23 +0530

    Change textStyle.

--- app/src/main/java/com/example/notify_1/MainActivity.java
+++ app/src/main/java/com/example/notify_1/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.core.app.NotificationManagerCompat;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -17,7 +19,7 @@ import static com.example.notify_1.App.CHANNEL_2_ID;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int NOTIFICATION_INTENT=1;
+    public static final int NOTIFICATION_INTENT = 1;
 
     //NotificationManagerCompat used here to allow backwards compatibility.
     NotificationManagerCompat notificationManagerCompat;
@@ -28,49 +30,69 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editTextTitle=(EditText)findViewById(R.id.edit_text_title);
-        editTextMessage=(EditText)findViewById(R.id.edit_text_message);
+        editTextTitle = (EditText) findViewById(R.id.edit_text_title);
+        editTextMessage = (EditText) findViewById(R.id.edit_text_message);
 
-        notificationManagerCompat=NotificationManagerCompat.from(this);
-
+        notificationManagerCompat = NotificationManagerCompat.from(this);
     }
-    public  void sendOnChannel1(View v){
-        String Title=editTextTitle.getText().toString();
-        String Message=editTextMessage.getText().toString();
+
+    public void sendOnChannel1(View v) {
+        String Title = editTextTitle.getText().toString();
+        String Message = editTextMessage.getText().toString();
 
         //For opening activity
-        Intent activityIntent=new Intent(this,MainActivity.class);
-        PendingIntent contentIntent =PendingIntent.getActivity(this,NOTIFICATION_INTENT,activityIntent,0);
+        Intent activityIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, NOTIFICATION_INTENT, activityIntent, 0);
 
         //For actions
-        Intent broadcastIntent=new Intent(this,NotificationReceiver.class);
-        broadcastIntent.putExtra("NOTIFICATION_MESSAGE",Message);
-        PendingIntent actionIntent=PendingIntent.getBroadcast(this,NOTIFICATION_INTENT,broadcastIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+        Intent broadcastIntent = new Intent(this, NotificationReceiver.class);
+        broadcastIntent.putExtra("NOTIFICATION_MESSAGE", Message);
+        PendingIntent actionIntent = PendingIntent.getBroadcast(this, NOTIFICATION_INTENT, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
+        //Converting image to bitmap
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.girl);
+
+        //Summary text remains same but content title and text changes as we toggle.
         //For api<26,notification attributes should also be defined here.
-        Notification notification=new NotificationCompat.Builder(this,CHANNEL_1_ID)
-                .setSmallIcon(R.drawable.ic_one)
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_one) //mandatory
                 .setContentTitle(Title)
                 .setContentText(Message)
+                .setLargeIcon(largeIcon)
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(getString(R.string.big_text))
+                        .setBigContentTitle("Big Content Title")
+                        .setSummaryText("Summary Text"))
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setColor(Color.BLUE)
                 .setContentIntent(contentIntent)
                 .setAutoCancel(true)
                 .setOnlyAlertOnce(true)
-                .addAction(R.mipmap.ic_launcher_round,"TOAST",actionIntent)
+                .addAction(R.mipmap.ic_launcher_round, "TOAST", actionIntent)
                 .build();
-        notificationManagerCompat.notify(1,notification);
+        notificationManagerCompat.notify(1, notification);
     }
-    public  void sendOnChannel2(View v){
-        String Title=editTextTitle.getText().toString();
-        String Message=editTextMessage.getText().toString();
 
-        Notification notification=new NotificationCompat.Builder(this,CHANNEL_2_ID)
+    public void sendOnChannel2(View v) {
+        String Title = editTextTitle.getText().toString();
+        String Message = editTextMessage.getText().toString();
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
                 .setSmallIcon(R.drawable.ic_two)
                 .setContentTitle(Title)
                 .setContentText(Message)
+                .setStyle(new NotificationCompat.InboxStyle()
+                        .addLine("This is line 1.")
+                        .addLine("This is line 1.")
+                        .addLine("This is line 1.")
+                        .addLine("This is line 1.")
+                        .addLine("This is line 1.")
+                        .addLine("This is line 1.")
+                        .addLine("This is line 1.")
+                        .setBigContentTitle("Big Content Title")
+                        .setSummaryText("Summary Text"))
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .build();
-        notificationManagerCompat.notify(2,notification);
+        notificationManagerCompat.notify(2, notification);
     }
 }
